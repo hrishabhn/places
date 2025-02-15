b6fcb582cd3a5c4ba1593c22fa48295372c91bf1
hrishabhn <hrishabhn@outlook.com>
2025-02-15 11:50:23 +0100

    Remove boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-import {ButtonExample} from './views/button'
-import {CalloutExample} from './views/callout'
-import {ColorsExample} from './views/colors'
-import {DrawerExample} from './views/drawer'
-import {FormExample} from './views/form'
-import {HeadingExample} from './views/heading'
-import {LoadingExample} from './views/loading'
-import {MenuExample} from './views/menu'
-import {TableExample} from './views/table'
-import {ThemesExample} from './views/themes'
-import {ToastExample} from './views/toast'
-import {TooltipExample} from './views/tooltip'
+import {PageHeader, PageSection} from '@/components/layout'
 
 export default function Home() {
     return (
-        <div className="space-y-4 py-4">
-            <HeadingExample />
-            <ColorsExample />
-            <ThemesExample />
-            <ButtonExample />
-            <MenuExample />
-            <FormExample />
-            <CalloutExample />
-            <TooltipExample />
-            <DrawerExample />
-            <TableExample />
-            <ToastExample />
-            <LoadingExample />
-        </div>
+        <>
+            <PageSection>
+                <PageHeader title="Places" />
+            </PageSection>
+        </>
     )
 }
