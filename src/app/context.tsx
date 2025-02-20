bae350598e499770d051f50b6a40b9c640509f0c
hrishabhn <hrishabhn@outlook.com>
2025-02-20 22:58:55 +0100

    Deprecate webhook

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import {revalidateNotion} from './action'
+
 import {type PlaceComplete} from '@/model/types'
 import {parseAsArrayOf, parseAsString, useQueryState} from 'nuqs'
-import {createContext, useCallback, useContext, useMemo} from 'react'
+import {createContext, useCallback, useContext, useEffect, useMemo} from 'react'
 
 import {kebabify} from '@/lib/kebab'
 import {type NotionSelect} from '@/lib/notion/types'
@@ -93,6 +95,10 @@ export function HomeContextProvider({
                 ),
         [allPlace, selectedCity, selectedType, selectedTags, cityIsSelected, typeIsSelected, tagIsSelected, query]
     )
+
+    useEffect(() => {
+        revalidateNotion()
+    }, [])
 
     return (
         <HomeContext.Provider
