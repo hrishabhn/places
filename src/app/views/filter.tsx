a6cb32beb7209068fc78a43f3008e9f1b0558e15
hrishabhn <hrishabhn@outlook.com>
2025-02-21 00:20:46 +0100

    Add filter bar on scroll

--- src/app/views/filter.tsx
+++ src/app/views/filter.tsx
@@ -5,12 +5,33 @@ import {useHomeContext} from '../context'
 
 import {database_id} from '@/model/config'
 import {ArrowCounterClockwise, City, Database, DotsThreeVertical, ForkKnife, GithubLogo, MapPin, Tag, Triangle} from '@phosphor-icons/react'
+import {useIntersectionObserver} from 'usehooks-ts'
 
 import {notionColorToTheme} from '@/lib/notion/color'
 
 import {DropdownDivider, DropdownHeader, DropdownMenuItem, DropdownMenuItems, FilterIcon, FilterItem, FilterTray, Menu, MenuButton} from '@/components/ui'
 
 export function HomeFilter() {
+    const {isIntersecting, ref} = useIntersectionObserver()
+
+    return (
+        <>
+            <div ref={ref} />
+
+            <FilterTray>
+                <HomeFilterContent />
+            </FilterTray>
+
+            {!isIntersecting && (
+                <div className="fixed left-0 top-0 z-10 flex w-full items-center gap-2 bg-gradient-to-b from-layer-0 from-25% to-layer-0/80 px-4 py-3 backdrop-blur dark:from-layer-0-dark dark:to-layer-0-dark/80">
+                    <HomeFilterContent />
+                </div>
+            )}
+        </>
+    )
+}
+
+function HomeFilterContent() {
     const {
         allCity,
         allType,
@@ -35,105 +56,98 @@ export function HomeFilter() {
 
     return (
         <>
-            <FilterTray>
-                <Menu>
-                    <MenuButton className="active:opacity-60">
-                        <FilterItem active={selectedCity.length > 0}>
-                            <City weight="duotone" />
-                            <p className="line-clamp-1">{selectedCity.length > 0 ? selectedCity.map(({name}) => name).join(', ') : 'City'}</p>
-                        </FilterItem>
-                    </MenuButton>
-                    <DropdownMenuItems>
-                        <DropdownMenuItem action={{onClick: () => clearSelectedCity()}} title="Show All" active={selectedCity.length === 0} />
-                        <DropdownDivider />
-                        {allCity.map(city => (
-                            <DropdownMenuItem
-                                key={city.id}
-                                action={{onClick: () => toggleSelectedCity(city)}}
-                                image={{theme: notionColorToTheme(city.color)}}
-                                title={city.name}
-                                active={cityIsSelected(city)}
-                            />
-                        ))}
-                    </DropdownMenuItems>
-                </Menu>
-
-                <Menu>
-                    <MenuButton className="active:opacity-60">
-                        <FilterItem active={selectedType.length > 0}>
-                            <ForkKnife weight="duotone" />
-                            <p className="line-clamp-1">{selectedType.length > 0 ? selectedType.map(({name}) => name).join(', ') : 'Type'}</p>
-                        </FilterItem>
-                    </MenuButton>
-                    <DropdownMenuItems>
-                        <DropdownMenuItem action={{onClick: () => clearSelectedType()}} title="Show All" active={selectedType.length === 0} />
-                        <DropdownDivider />
-                        {allType.map(type => (
-                            <DropdownMenuItem
-                                key={type.id}
-                                action={{onClick: () => toggleSelectedType(type)}}
-                                image={{theme: notionColorToTheme(type.color)}}
-                                title={type.name}
-                                active={typeIsSelected(type)}
-                            />
-                        ))}
-                    </DropdownMenuItems>
-                </Menu>
-
-                <Menu>
-                    <MenuButton className="active:opacity-60">
-                        <FilterItem active={selectedTags.length > 0}>
-                            <Tag weight="duotone" />
-                            <p className="line-clamp-1">{selectedTags.length > 0 ? selectedTags.map(({name}) => name).join(', ') : 'Tags'}</p>
-                        </FilterItem>
-                    </MenuButton>
-                    <DropdownMenuItems>
-                        <DropdownMenuItem action={{onClick: () => clearSelectedTags()}} title="Show All" active={selectedTags.length === 0} />
-                        <DropdownDivider />
-                        {allTags.map(tag => (
-                            <DropdownMenuItem
-                                key={tag.id}
-                                action={{onClick: () => toggleSelectedTag(tag)}}
-                                image={{theme: notionColorToTheme(tag.color)}}
-                                title={tag.name}
-                                active={tagIsSelected(tag)}
-                            />
-                        ))}
-                    </DropdownMenuItems>
-                </Menu>
-
-                <div className="grow" />
-
-                <Menu>
-                    <MenuButton>
-                        <FilterIcon icon={DotsThreeVertical} />
-                    </MenuButton>
-                    <DropdownMenuItems anchor="bottom end">
-                        <DropdownMenuItem action={{onClick: async () => await revalidateNotion()}} image={{icon: ArrowCounterClockwise}} title="Refresh Data" />
-
-                        <DropdownDivider />
-
-                        <DropdownMenuItem action={{href: `https://notion.so/${database_id}`, target: '_blank'}} image={{icon: Database}} title="Database" subtitle="Notion" />
+            <Menu>
+                <MenuButton className="active:opacity-60">
+                    <FilterItem active={selectedCity.length > 0}>
+                        <City weight="duotone" />
+                        <p className="line-clamp-1">{selectedCity.length > 0 ? selectedCity.map(({name}) => name).join(', ') : 'City'}</p>
+                    </FilterItem>
+                </MenuButton>
+                <DropdownMenuItems>
+                    <DropdownMenuItem action={{onClick: () => clearSelectedCity()}} title="Show All" active={selectedCity.length === 0} />
+                    <DropdownDivider />
+                    {allCity.map(city => (
                         <DropdownMenuItem
-                            action={{href: 'https://developers.google.com/maps/documentation/places/web-service/place-id', target: '_blank'}}
-                            image={{icon: MapPin}}
-                            title="Place ID"
-                            subtitle="Google Maps"
+                            key={city.id}
+                            action={{onClick: () => toggleSelectedCity(city)}}
+                            image={{theme: notionColorToTheme(city.color)}}
+                            title={city.name}
+                            active={cityIsSelected(city)}
                         />
+                    ))}
+                </DropdownMenuItems>
+            </Menu>
 
-                        <DropdownDivider />
-
-                        <DropdownHeader text="Resources" />
+            <Menu>
+                <MenuButton className="active:opacity-60">
+                    <FilterItem active={selectedType.length > 0}>
+                        <ForkKnife weight="duotone" />
+                        <p className="line-clamp-1">{selectedType.length > 0 ? selectedType.map(({name}) => name).join(', ') : 'Type'}</p>
+                    </FilterItem>
+                </MenuButton>
+                <DropdownMenuItems>
+                    <DropdownMenuItem action={{onClick: () => clearSelectedType()}} title="Show All" active={selectedType.length === 0} />
+                    <DropdownDivider />
+                    {allType.map(type => (
                         <DropdownMenuItem
-                            action={{href: 'https://github.com/hrishabhn/places', target: '_blank'}}
-                            image={{icon: GithubLogo}}
-                            title="Source Code"
-                            subtitle="GitHub"
+                            key={type.id}
+                            action={{onClick: () => toggleSelectedType(type)}}
+                            image={{theme: notionColorToTheme(type.color)}}
+                            title={type.name}
+                            active={typeIsSelected(type)}
                         />
-                        <DropdownMenuItem action={{href: 'https://vercel.com/hrishabhn/places', target: '_blank'}} image={{icon: Triangle}} title="Preview" subtitle="Vercel" />
-                    </DropdownMenuItems>
-                </Menu>
-            </FilterTray>
+                    ))}
+                </DropdownMenuItems>
+            </Menu>
+
+            <Menu>
+                <MenuButton className="active:opacity-60">
+                    <FilterItem active={selectedTags.length > 0}>
+                        <Tag weight="duotone" />
+                        <p className="line-clamp-1">{selectedTags.length > 0 ? selectedTags.map(({name}) => name).join(', ') : 'Tags'}</p>
+                    </FilterItem>
+                </MenuButton>
+                <DropdownMenuItems>
+                    <DropdownMenuItem action={{onClick: () => clearSelectedTags()}} title="Show All" active={selectedTags.length === 0} />
+                    <DropdownDivider />
+                    {allTags.map(tag => (
+                        <DropdownMenuItem
+                            key={tag.id}
+                            action={{onClick: () => toggleSelectedTag(tag)}}
+                            image={{theme: notionColorToTheme(tag.color)}}
+                            title={tag.name}
+                            active={tagIsSelected(tag)}
+                        />
+                    ))}
+                </DropdownMenuItems>
+            </Menu>
+
+            <div className="grow" />
+
+            <Menu>
+                <MenuButton>
+                    <FilterIcon icon={DotsThreeVertical} />
+                </MenuButton>
+                <DropdownMenuItems anchor="bottom end">
+                    <DropdownMenuItem action={{onClick: async () => await revalidateNotion()}} image={{icon: ArrowCounterClockwise}} title="Refresh Data" />
+
+                    <DropdownDivider />
+
+                    <DropdownMenuItem action={{href: `https://notion.so/${database_id}`, target: '_blank'}} image={{icon: Database}} title="Database" subtitle="Notion" />
+                    <DropdownMenuItem
+                        action={{href: 'https://developers.google.com/maps/documentation/places/web-service/place-id', target: '_blank'}}
+                        image={{icon: MapPin}}
+                        title="Place ID"
+                        subtitle="Google Maps"
+                    />
+
+                    <DropdownDivider />
+
+                    <DropdownHeader text="Resources" />
+                    <DropdownMenuItem action={{href: 'https://github.com/hrishabhn/places', target: '_blank'}} image={{icon: GithubLogo}} title="Source Code" subtitle="GitHub" />
+                    <DropdownMenuItem action={{href: 'https://vercel.com/hrishabhn/places', target: '_blank'}} image={{icon: Triangle}} title="Preview" subtitle="Vercel" />
+                </DropdownMenuItems>
+            </Menu>
         </>
     )
 }
