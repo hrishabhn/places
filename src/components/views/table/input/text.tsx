af96f64b36634037e35c8813e9853bdd18b236f2
hrishabhn <hrishabhn@outlook.com>
2025-02-22 18:55:37 +0100

    fix

--- src/components/views/table/input/text.tsx
+++ src/components/views/table/input/text.tsx
@@ -8,11 +8,10 @@ import {type ChangeEventHandler} from 'react'
 import {inputDebounce} from '@/lib/debounce'
 
 type TableTextInputProps = {
-    multiline?: boolean
     debounce?: boolean
 } & TableInputProps<string>
 
-export function TableTextInput({multiline, debounce = false, size, defaultValue, value, setValue}: TableTextInputProps) {
+export function TableTextInput({debounce = false, size, defaultValue, value, setValue}: TableTextInputProps) {
     const onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined = (() => {
         if (setValue === undefined) return undefined
         const eventHandler: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = e => setValue(e.target.value.trim())
