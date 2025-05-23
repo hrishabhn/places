315f066249675277312511784b29e1c35d66ce13
hrishabhn <hrishabhn@outlook.com>
2025-05-23 15:20:37 +0200

    Add `@vercel/speed-insights`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import {Providers} from './providers'
 
 import {Analytics} from '@vercel/analytics/next'
+import {SpeedInsights} from '@vercel/speed-insights/next'
 import type {Metadata, Viewport} from 'next'
 import {Inter, Lora} from 'next/font/google'
 
@@ -30,7 +31,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <Analytics />
-
+            <SpeedInsights />
             <Providers>
                 <body className={`${inter.className} ${serif.variable} flex min-h-screen w-full flex-col bg-layer-0 text-black antialiased dark:bg-layer-0-dark dark:text-white`}>
                     <Navbar />
