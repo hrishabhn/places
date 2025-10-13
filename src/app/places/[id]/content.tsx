30a16b64252771104cb79b24b6703ec743173e6b
hrishabhn <hrishabhn@outlook.com>
2025-10-13 17:15:43 +0200

    Update detail page

--- src/app/places/[id]/content.tsx
+++ src/app/places/[id]/content.tsx
@@ -1,26 +1,73 @@
 'use client'
 
-import {useQuery} from '@tanstack/react-query'
+import {HeartIcon, MapTrifoldIcon, PencilIcon} from '@phosphor-icons/react'
+import {useMutation, useQueryClient, useSuspenseQuery} from '@tanstack/react-query'
+import Link from 'next/link'
 import {notFound} from 'next/navigation'
+
+import {getBookmarks, toggleBookmark} from '@/model/bookmarks'
+import {googleMapsUrl, notionUrl} from '@/model/util'
 
 import {useTRPC} from '@/lib/trpc'
 
-import {Loading} from '@/components/views/loading'
+import {Heading} from '@/components/layout'
+import {MenuBarItem, MenuBarTray} from '@/components/views/menu-bar'
+import {Section} from '@/components/views/section'
 import {Splash} from '@/components/views/splash'
-import {DetailStack} from '@/components/views/stack'
+import {PageStack} from '@/components/views/stack'
 
 export function PlacePageContent({id}: {id: string}) {
     const trpc = useTRPC()
-    const {status: placeStatus, data: place} = useQuery(trpc.GetPlace.queryOptions({id}))
+    const queryClient = useQueryClient()
 
-    if (placeStatus === 'pending') return <Loading />
-    if (placeStatus === 'error') throw new Error('Failed to load place')
+    const {data: place} = useSuspenseQuery(trpc.GetPlace.queryOptions({id}))
+
+    const {data: bookmarks} = useSuspenseQuery({
+        queryKey: ['bookmarks'],
+        queryFn: async () => await getBookmarks(),
+    })
+    const {mutate: toggle} = useMutation({mutationFn: async (id: string) => await queryClient.setQueryData(['bookmarks'], await toggleBookmark(id))})
 
     if (place === null) return notFound()
 
     return (
-        <DetailStack padding>
-            <Splash title={place.name} subtitle={place.city_name} image={place.image ?? undefined} />
-        </DetailStack>
+        <PageStack>
+            <Splash title={place.name} subtitle={place.city_name} description={place.description ?? undefined} image={place.image ?? undefined} />
+
+            <Section>
+                <Heading size="h2" serif>
+                    Links
+                </Heading>
+            </Section>
+
+            <MenuBarTray>
+                <Link href={googleMapsUrl({name: place.name, city_name: place.city_name, maps_id: place.maps_id})} target="_blank">
+                    <MenuBarItem>
+                        <MapTrifoldIcon weight="bold" />
+                        <p>Open in Google Maps</p>
+                    </MenuBarItem>
+                </Link>
+                <Link href={notionUrl(place.id)} target="_blank">
+                    <MenuBarItem>
+                        <PencilIcon weight="bold" />
+                        <p>Edit</p>
+                    </MenuBarItem>
+                </Link>
+            </MenuBarTray>
+
+            <Section>
+                <Heading size="h2" serif>
+                    Actions
+                </Heading>
+            </Section>
+            <MenuBarTray>
+                <button onClick={() => toggle(place.id)} className="active:opacity-60">
+                    <MenuBarItem active={bookmarks.includes(place.id)}>
+                        <HeartIcon weight={bookmarks.includes(place.id) ? 'fill' : 'bold'} />
+                        <p>Bookmark</p>
+                    </MenuBarItem>
+                </button>
+            </MenuBarTray>
+        </PageStack>
     )
 }
--- src/components/views/splash.tsx
+++ src/components/views/splash.tsx
@@ -3,14 +3,14 @@ import {HLine} from '@/components/views/h-line'
 import {Section} from '@/components/views/section'
 import {PageStack} from '@/components/views/stack'
 
-export function Splash({title, subtitle, image}: {title: string; subtitle?: string; image?: string}) {
+export function Splash({title, subtitle, description, image}: {title: string; subtitle?: string; description?: string; image?: string}) {
     return (
         <Section>
             <PageStack>
                 <HLine />
                 <div className="grid auto-cols-fr grid-flow-row gap-6 lg:grid-flow-col">
                     <div className="size-full">
-                        <SplashTextbox title={title} subtitle={subtitle} />
+                        <SplashTextbox title={title} subtitle={subtitle} description={description} />
                     </div>
                     <div className="aspect-video w-full">
                         <SimpleImage url={image} alt={title} />
@@ -21,11 +21,12 @@ export function Splash({title, subtitle, image}: {title: string; subtitle?: stri
     )
 }
 
-export function SplashTextbox({title, subtitle}: {title: string; subtitle?: string}) {
+export function SplashTextbox({title, subtitle, description}: {title: string; subtitle?: string; description?: string}) {
     return (
         <>
             <p className="line-clamp-3 font-heading text-6xl lg:text-9xl">{title}</p>
             {subtitle ? <p className="line-clamp-3 font-heading text-4xl lg:text-6xl">{subtitle}</p> : null}
+            {description ? <p className="line-clamp-5 font-serif text-xl lg:text-2xl">{description}</p> : null}
         </>
     )
 }
