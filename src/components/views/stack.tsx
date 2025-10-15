ba4d07813707dfc0a08379ac86e406bbc2031f44
hrishabhn <hrishabhn@outlook.com>
2025-10-15 18:03:04 +0200

    fix page stack

--- src/components/views/stack.tsx
+++ src/components/views/stack.tsx
@@ -1,7 +1,3 @@
 export function PageStack({children}: {children?: React.ReactNode}) {
-    return (
-        <div className="min-h-screen">
-            <div className="grid auto-cols-auto grid-flow-row gap-6 py-6 sm:gap-8 sm:py-8">{children}</div>
-        </div>
-    )
+    return <div className="grid auto-cols-auto grid-flow-row gap-6 py-6 sm:gap-8 sm:py-8">{children}</div>
 }
