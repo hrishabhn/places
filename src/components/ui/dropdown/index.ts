2b7423ef38718333091189b819211da1c86d135e
hrishabhn <hrishabhn@outlook.com>
2025-10-16 10:49:25 +0200

    move dropdown animation to framer

--- src/components/ui/dropdown/index.ts
+++ src/components/ui/dropdown/index.ts
@@ -1,7 +1,5 @@
-export {DropdownTransition} from './transition'
-
 export {DropdownItem, type DropdownItemProps} from './item'
 export {DropdownDivider} from './divider'
 export {DropdownHeader} from './header'
 
-export type DropdownAnchor = `${'top' | 'bottom'} ${'start' | 'end'}`
+export type DropdownAnchor = `${'bottom'} ${'start' | 'end'}`
