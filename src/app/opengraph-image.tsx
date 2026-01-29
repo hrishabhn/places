7460d2687401328438eaed4b1e120a0f49d183ac
hrishabhn <hrishabhn@outlook.com>
2026-01-29 23:58:17 +0100

    Change opengraph image font

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -35,116 +35,114 @@ const stack = (flexDirection: 'row' | 'column' = 'column'): CSSProperties => ({
 // Image generation
 export default async function Image() {
     return new ImageResponse(
-        (
+        <div
+            style={{
+                ...stack(),
+                height: '100%',
+                width: '100%',
+                padding: '1.5em',
+                paddingBottom: '0',
+                background: colors.zinc[300],
+                fontSize: 64,
+            }}
+        >
             <div
                 style={{
                     ...stack(),
-                    height: '100%',
                     width: '100%',
-                    padding: '1.5em',
-                    paddingBottom: '0',
-                    background: colors.zinc[300],
-                    fontSize: 64,
+                    flexGrow: 1,
+                    backgroundColor: colors.white,
+                    borderRadius: '0.5em 0.5em 0 0',
+                    boxShadow: '0 0 32px #00000033',
+                    overflow: 'hidden',
+                    color: 'white',
+                    fontWeight: 'bold',
                 }}
             >
+                <div
+                    style={{
+                        display: 'flex',
+                        width: '100%',
+                        backgroundColor: accent,
+                        color: 'white',
+                        flexShrink: 0,
+                        padding: '0.5em 1em',
+                    }}
+                >
+                    <p style={{margin: 0, fontSize: '1.5em'}}>{appTitle}</p>
+                </div>
+
                 <div
                     style={{
                         ...stack(),
                         width: '100%',
                         flexGrow: 1,
-                        backgroundColor: colors.white,
-                        borderRadius: '0.5em 0.5em 0 0',
-                        boxShadow: '0 0 32px #00000033',
-                        overflow: 'hidden',
-                        color: 'white',
-                        fontWeight: 'bold',
+                        flexBasis: 0,
+                        gap: '0.75em',
+                        padding: '0.5em 1em',
+                        paddingBottom: 0,
                     }}
                 >
                     <div
                         style={{
-                            display: 'flex',
-                            width: '100%',
-                            backgroundColor: accent,
-                            color: 'white',
-                            flexShrink: 0,
-                            padding: '0.5em 1em',
-                        }}
-                    >
-                        <p style={{margin: 0, fontSize: '1.5em'}}>{appTitle}</p>
-                    </div>
-
-                    <div
-                        style={{
-                            ...stack(),
+                            ...stack('row'),
                             width: '100%',
                             flexGrow: 1,
                             flexBasis: 0,
-                            gap: '0.75em',
-                            padding: '0.5em 1em',
-                            paddingBottom: 0,
+                            gap: '0.5em',
                         }}
                     >
-                        <div
-                            style={{
-                                ...stack('row'),
-                                width: '100%',
-                                flexGrow: 1,
-                                flexBasis: 0,
-                                gap: '0.5em',
-                            }}
-                        >
-                            {Array.from({length: 3}).map((_, i) => (
-                                <div
-                                    key={i}
-                                    style={{
-                                        flexGrow: 1,
-                                        flexBasis: 0,
-                                        height: '100%',
-                                        padding: '1em',
-                                        border: `1px solid ${colors.zinc[200]}`,
-                                        borderRadius: '0.5em',
-                                        backgroundColor: colors.zinc[100],
-                                    }}
-                                />
-                            ))}
-                        </div>
-                        <div
-                            style={{
-                                ...stack('row'),
-                                width: '100%',
-                                flexGrow: 0.5,
-                                flexBasis: 0,
-                                gap: '0.5em',
-                            }}
-                        >
-                            {Array.from({length: 3}).map((_, i) => (
-                                <div
-                                    key={i}
-                                    style={{
-                                        flexGrow: 1,
-                                        flexBasis: 0,
-                                        height: '100%',
-                                        padding: '1em',
-                                        border: `1px solid ${colors.zinc[200]}`,
-                                        borderBottom: 'none',
-                                        borderRadius: '0.5em 0.5em 0 0',
-                                        backgroundColor: colors.zinc[100],
-                                    }}
-                                />
-                            ))}
-                        </div>
+                        {Array.from({length: 3}).map((_, i) => (
+                            <div
+                                key={i}
+                                style={{
+                                    flexGrow: 1,
+                                    flexBasis: 0,
+                                    height: '100%',
+                                    padding: '1em',
+                                    border: `1px solid ${colors.zinc[200]}`,
+                                    borderRadius: '0.5em',
+                                    backgroundColor: colors.zinc[100],
+                                }}
+                            />
+                        ))}
+                    </div>
+                    <div
+                        style={{
+                            ...stack('row'),
+                            width: '100%',
+                            flexGrow: 0.5,
+                            flexBasis: 0,
+                            gap: '0.5em',
+                        }}
+                    >
+                        {Array.from({length: 3}).map((_, i) => (
+                            <div
+                                key={i}
+                                style={{
+                                    flexGrow: 1,
+                                    flexBasis: 0,
+                                    height: '100%',
+                                    padding: '1em',
+                                    border: `1px solid ${colors.zinc[200]}`,
+                                    borderBottom: 'none',
+                                    borderRadius: '0.5em 0.5em 0 0',
+                                    backgroundColor: colors.zinc[100],
+                                }}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
-        ),
+        </div>,
         {
             ...size,
             fonts: [
                 {
-                    name: 'Roboto Slab',
-                    data: await readFile(join(process.cwd(), 'src/public/roboto-slab/RobotoSlab-Bold.ttf')),
+                    name: 'Gupter Medium',
+                    data: await readFile(join(process.cwd(), 'src/public/gupter/Gupter-Medium.ttf')),
                     style: 'normal',
-                    weight: 700,
+                    weight: 500,
                 },
             ],
         }
