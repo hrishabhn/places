eb873a4e9451b5fb3c0eb57370f7848b4ec25913
hrishabhn <hrishabhn@outlook.com>
2026-03-15 17:07:01 +0000

    fix lint error

--- src/components/views/menu-bar/item.tsx
+++ src/components/views/menu-bar/item.tsx
@@ -1,7 +1,7 @@
 export function MenuBarItem({active = false, children}: {active?: boolean; children?: React.ReactNode}) {
     return (
         <div
-            className={`flex items-center gap-2 rounded-full border-2 px-2.5 py-1.5 text-sm font-semibold text-nowrap ${active ? 'border-transparent bg-accent-dark text-accent-light dark:bg-accent-light dark:text-accent-dark' : 'border-transparent  text-accent-dark bg-accent-dark/10 dark:text-accent-light dark:bg-accent-light/10'}`}
+            className={`flex items-center gap-2 rounded-full border-2 px-2.5 py-1.5 text-sm font-semibold text-nowrap ${active ? 'border-transparent bg-accent-dark text-accent-light dark:bg-accent-light dark:text-accent-dark' : 'border-transparent bg-accent-dark/10 text-accent-dark dark:bg-accent-light/10 dark:text-accent-light'}`}
         >
             {children}
         </div>
